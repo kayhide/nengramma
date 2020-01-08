8a2fb394b92e5f6483d3ea558c1fd2d58c877ecb
Hideaki Kawai <kayhide@gmail.com>
2020-01-08 15:30:49 +0900

    Create Layout

---
+++ src/App/View/Layout.purs
@@ -0,0 +1,21 @@
+module App.View.Layout where
+
+import Prelude
+
+import Effect (Effect)
+import React.Basic.DOM as R
+import React.Basic.Events (handler_)
+import React.Basic.Hooks (ReactComponent, component, fragment, useState, (/\))
+import React.Basic.Hooks as React
+
+make :: Effect (ReactComponent {})
+make = do
+  component "Counter" \props -> React.do
+    counter /\ setCounter <- useState 0
+    pure
+      $ fragment
+        [ R.button
+            { onClick: handler_ $ setCounter (_ + 1)
+            , children: [ R.text $ "Increment: " <> show counter ]
+            }
+        ]
--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,24 @@ module Main where
 
 import Prelude
 
+import App.View.Layout as Layout
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Exception (throw)
+import React.Basic.DOM (render)
+import React.Basic.Hooks (element)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  window
+    >>= document
+    >>= toNonElementParentNode >>> getElementById "app"
+    >>= case _ of
+    Nothing -> throw "Container element not found."
+    Just c  -> do
+      layout <- Layout.make
+      render (element layout {}) c
